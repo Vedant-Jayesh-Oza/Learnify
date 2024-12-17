87a8ae7dc1ce26bb7a09068b10fd031357f73672
Vedant-Jayesh-Oza <vedantoza1313@gmail.com>
2024-12-17 12:38:18 -0600

    feat: implement Stripe payment checkout flow

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -94,11 +94,44 @@ export const api = createApi({
       query: (id) => `courses/${id}`,
       providesTags: (result, error, id) => [{ type: "Courses", id }],
     }),
+
+    /* 
+    ===============
+    TRANSACTIONS
+    =============== 
+    */
+
+    getTransactions: build.query<Transaction[], string>({
+      query: (userId) => `transactions?userId=${userId}`,
+    }),
+
+    createStripePaymentIntent: build.mutation<
+      { clientSecret: string },
+      { amount: number }
+    >({
+      query: ({ amount }) => ({
+        url: `/transactions/stripe/payment-intent`,
+        method: "POST",
+        body: { amount },
+      }),
+    }),
+
+    createTransaction: build.mutation<Transaction, Partial<Transaction>>({
+      query: (transaction) => ({
+        url: "transactions",
+        method: "POST",
+        body: transaction,
+      }),
+    }),
+
   }),
 });    
  
 export const {
   useUpdateUserMutation,
   useGetCoursesQuery,
-  useGetCourseQuery
+  useGetCourseQuery,
+  useGetTransactionsQuery,
+  useCreateTransactionMutation,
+  useCreateStripePaymentIntentMutation,
 } = api;
